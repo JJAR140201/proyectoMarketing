192c7fc470616ae56114e02fa8f9c3a54c7bf4b5
JJAR140201 <avilesroajuanjose@gmail.com>
2023-02-27 15:01:54 -0500

    se creo modelo detalle orden
    Aqui va a tener todo los detalles de la venta que se vayan a realizar

---
+++ src/main/java/com/proyecto/poryectoMarketing/Model/detalleOrden.java
@@ -0,0 +1,71 @@
+package com.proyecto.poryectoMarketing.Model;
+
+public class detalleOrden {
+    private Integer id;
+    private Integer nombre;
+    private Integer cantidad;
+    private Integer precio;
+    private Integer total;
+
+    public detalleOrden(){
+
+    }
+    public detalleOrden(Integer id, Integer nombre, Integer cantidad, Integer precio, Integer total) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "detalleOrden{" +
+                "id=" + id +
+                ", nombre=" + nombre +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                ", total=" + total +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(Integer nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
