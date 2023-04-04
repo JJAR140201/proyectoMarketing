23669ac43fe9690d2009b24221bd72186eaf9e21
JJAR140201 <avilesroajuanjose@gmail.com>
2023-04-04 08:48:56 -0500

    Creamos los metodos guardar, crear, actualizar y eliminar como deberia ser la logica de negocio

---
+++ src/main/java/com/proyecto/poryectoMarketing/Service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.proyecto.poryectoMarketing.Service;
+
+import com.proyecto.poryectoMarketing.Model.Productos;
+
+import java.util.Optional;
+
+public interface ProductoService {
+    public Productos guardar( Productos productos);
+    public Optional <Productos> get(Integer id);
+    public Productos update(Productos productos);
+    public void delete(Integer id);
+}
