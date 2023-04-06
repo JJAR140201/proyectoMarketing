924c41ce57ddc00e96dd5274b50e5effe49c3e0c
JJAR140201 <avilesroajuanjose@gmail.com>
2023-04-06 11:02:32 -0500

    Creamos metodo para guardar producto asignando un usuario

--- src/main/java/com/proyecto/poryectoMarketing/Controllers/ProductoController.java
+++ src/main/java/com/proyecto/poryectoMarketing/Controllers/ProductoController.java
@@ -1,12 +1,24 @@
 package com.proyecto.poryectoMarketing.Controllers;
 
+import com.proyecto.poryectoMarketing.Model.Productos;
+import com.proyecto.poryectoMarketing.Model.Usuario;
+import com.proyecto.poryectoMarketing.Service.ProductoService;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(String.valueOf(ProductoController.class));
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show() {
@@ -16,4 +28,11 @@ public class ProductoController {
     @GetMapping("/create")
     public  String create() { return  "productos/create";}
 
+    @PostMapping("/save")
+    public String save(Productos productos) {
+        LOGGER.info("Este es el objeto de vista {}");
+        Usuario u = new Usuario();
+        productos.setUsuario(u);
+        productoService.guardar(productos);
+        return "redirect:/productos";}
 }
