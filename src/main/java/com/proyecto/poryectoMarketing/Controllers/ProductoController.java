345fd8864d0a11c6b32a7cb0aa8b65e1c8cc74c1
JJAR140201 <avilesroajuanjose@gmail.com>
2023-04-09 10:16:42 -0500

    Implementacion de funcionalidad para mostrar productos administrador

--- src/main/java/com/proyecto/poryectoMarketing/Controllers/ProductoController.java
+++ src/main/java/com/proyecto/poryectoMarketing/Controllers/ProductoController.java
@@ -3,25 +3,25 @@ package com.proyecto.poryectoMarketing.Controllers;
 import com.proyecto.poryectoMarketing.Model.Productos;
 import com.proyecto.poryectoMarketing.Model.Usuario;
 import com.proyecto.poryectoMarketing.Service.ProductoService;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
-    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(String.valueOf(ProductoController.class));
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
     @Autowired
     private ProductoService productoService;
 
     @GetMapping("")
-    public String show() {
+    public String show(Model model) {
+        model.addAttribute("productos",productoService.findAll() );
         return "productos/show";
     }
 
@@ -31,7 +31,7 @@ public class ProductoController {
     @PostMapping("/save")
     public String save(Productos productos) {
         LOGGER.info("Este es el objeto de vista {}");
-        Usuario u = new Usuario();
+        Usuario u = new Usuario(1, "", "", "", "","","","","");
         productos.setUsuario(u);
         productoService.guardar(productos);
         return "redirect:/productos";}
--- src/main/java/com/proyecto/poryectoMarketing/Service/ProductoService.java
+++ src/main/java/com/proyecto/poryectoMarketing/Service/ProductoService.java
@@ -2,6 +2,7 @@ package com.proyecto.poryectoMarketing.Service;
 
 import com.proyecto.poryectoMarketing.Model.Productos;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoService {
@@ -9,4 +10,5 @@ public interface ProductoService {
     public Optional <Productos> get(Integer id);
     public Productos update(Productos productos);
     public void delete(Integer id);
+    public List<Productos> findAll();
 }
